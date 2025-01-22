20ef60536faf911f82826d12334e173a00b15ee5
ThanasPapa <100229861+Slowbro213@users.noreply.github.com>
2025-01-22 22:53:35 +0100

    flower-new

--- server.py
+++ server.py
@@ -1,44 +1,17 @@
-import numpy as np
-import tensorflow as tf
 import flwr as fl
 
-# Load dataset for this client
-client_id = 0  # Change this for each VM (0, 1, 2, ...)
-data = np.load(f"client_{client_id}.npz")
-X_train, y_train = data["X_train"], data["y_train"]
-X_test, y_test = data["X_test"], data["y_test"]
+# Define strategy (e.g., Federated Averaging)
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1.0,  # Sample 100% of clients for training
+    fraction_evaluate=1.0,  # Sample 100% of clients for evaluation
+    min_fit_clients=3,  # Minimum number of clients to train
+    min_evaluate_clients=3,  # Minimum number of clients to evaluate
+    min_available_clients=3,  # Minimum number of clients available
+)
 
-# Convert to TensorFlow datasets
-train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).batch(32)
-test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(32)
-
-# Define a simple neural network
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation="relu", input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dense(32, activation="relu"),
-    tf.keras.layers.Dense(len(np.unique(y_train)), activation="softmax"),
-])
-
-# Compile the model
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-
-# Define Flower client
-class NetflixClient(fl.client.NumPyClient):
-    def get_parameters(self, config):
-        return model.get_weights()
-
-    def set_parameters(self, parameters):
-        model.set_weights(parameters)
-
-    def fit(self, parameters, config):
-        self.set_parameters(parameters)
-        model.fit(train_dataset, epochs=5, verbose=0)  # Train for 5 epochs
-        return self.get_parameters(config), len(X_train), {}
-
-    def evaluate(self, parameters, config):
-        self.set_parameters(parameters)
-        loss, accuracy = model.evaluate(test_dataset, verbose=0)
-        return loss, len(X_test), {"accuracy": accuracy}
-
-# Start Flower client
-fl.client.start_numpy_client(server_address="[SERVER_IP]:8080", client=NetflixClient())
+# Start Flower server
+fl.server.start_server(
+    server_address="0.0.0.0:8080",
+    config=fl.server.ServerConfig(num_rounds=10),
+    strategy=strategy,
+)
